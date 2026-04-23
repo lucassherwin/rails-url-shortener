9e73a7f2c717937213830408be710ebc116f3e9c
Lucas Sherwin <lksherwin@gmail.com>
2026-04-23 16:38:33 -0400

    Implement user authentication and password reset functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get 'hello_world', to: 'hello_world#index'
   root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,4 +15,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resource :sign_up
 end
